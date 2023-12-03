3976d93eb6baf9d4397aa3d89264f7fade5fabef
DandinPower <tomhot246@gmail.com>
2023-12-03 16:43:36 +0800

    test: add some test case

---
+++ test/test_openmp_spmm.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+from src.utils import generate_sparse_matrix
+from src.extension import ExtensionHandler
+
+@pytest.fixture
+def handler():
+    return ExtensionHandler()
+
+@pytest.mark.parametrize("dim1,dim2,density", [(5, 4, 0.7), (10, 10, 0.5), (3, 3, 0.9)])
+def test_openmp_sparse_mm(handler, dim1, dim2, density):
+    # Generate two sparse matrices
+    sparse_matrix1 = generate_sparse_matrix(dim1, dim2, density=density)
+    sparse_matrix2 = generate_sparse_matrix(dim2, dim1, density=density)
+
+    # Multiply the sparse matrices using your function
+    result_sparse: torch.Tensor = handler.openmp_sparse_mm(sparse_matrix1, sparse_matrix2)
+    result_sparse_true: torch.Tensor = torch.sparse.mm(sparse_matrix1, sparse_matrix2)
+
+    # Check if the result from your function matches the expected result
+    assert torch.allclose(result_sparse.to_dense(), result_sparse_true.to_dense())
